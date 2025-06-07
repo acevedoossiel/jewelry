9998d9f48eab2b961c4c94278cc379f48ca7cebf
acevedoossiel <acevedoossiel@gmail.com>
2025-06-06 21:04:14 -0600

    cambio3

--- jewelry-app/lib/models/jewelry_model.dart
+++ jewelry-app/lib/models/jewelry_model.dart
@@ -1,40 +1,47 @@
 class JewelryModel {
+  String? mongoId; // 👈 Nuevo: ID real de Mongo (_id)
   int id;
   String name;
   String material;
-  List<String> mediaLinks; // 👈 CAMBIADO: ahora es una lista
+  List<String> mediaLinks;
   List<String> details;
   double price;
   int quantity;
+  bool isFavorite;
 
   JewelryModel({
+    this.mongoId, // 👈 Nuevo
     required this.id,
     required this.name,
     required this.material,
-    required this.mediaLinks, // 👈 CAMBIO
+    required this.mediaLinks,
     required this.details,
     required this.price,
     this.quantity = 1,
+    this.isFavorite = false,
   });
 
   factory JewelryModel.fromJSON(Map<String, dynamic> json) {
     return JewelryModel(
+      mongoId: json['_id'], // 👈 Extraído del backend
       id: json['id'],
       name: json['name'],
       material: json['material'],
-      mediaLinks: List<String>.from(json['mediaLinks']), // 👈 CAMBIO
+      mediaLinks: List<String>.from(json['mediaLinks']),
       details: List<String>.from(json['details']),
       price: (json['price'] as num).toDouble(),
       quantity: json['quantity'] ?? 1,
+      isFavorite: json['isFavorite'] ?? false,
     );
   }
 
   Map<String, dynamic> toJSON() {
     return {
+      // No incluimos _id aquí porque no es necesario al enviar JSON
       'id': id,
       'name': name,
       'material': material,
-      'mediaLinks': mediaLinks, // 👈 CAMBIO
+      'mediaLinks': mediaLinks,
       'details': details,
       'price': price,
       'quantity': quantity,
@@ -43,6 +50,6 @@ class JewelryModel {
 
   @override
   String toString() {
-    return 'Jewelry(id: $id, name: $name, material: $material, mediaLinks: $mediaLinks, price: $price, quantity: $quantity, details: $details)';
+    return 'Jewelry(_id: $mongoId, id: $id, name: $name, material: $material, mediaLinks: $mediaLinks, price: $price, quantity: $quantity, details: $details, isFavorite: $isFavorite)';
   }
 }
